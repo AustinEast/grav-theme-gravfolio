d9e196584dce1248d5ebd2340f8feacaabe75483
austin <eastin93@gmail.com>
2018-09-30 21:08:11 -0400

    Slider items are now centered

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,14 @@
 var mySwiper = new Swiper('.swiper-container', {
 	loop: true,
+	centeredSlides: true,
+	slidesPerView: 1,
+	paginationClickable: true,
+	autoHeight: true,
 	pagination: {
 		el: '.swiper-pagination',
 	},
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
-	},
+	}
 });
